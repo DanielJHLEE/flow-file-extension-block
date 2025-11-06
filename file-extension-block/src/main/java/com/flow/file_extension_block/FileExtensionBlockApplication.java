7662660225cb5fcbb9b049454d23959ab774d99e
leeJH <ljh0200@outlook.kr>
2025-11-06 20:16:39 +0900

    Add Spring Boot Flow-File-Extension-Block project initialization

---
+++ file-extension-block/src/main/java/com/flow/file_extension_block/FileExtensionBlockApplication.java
@@ -0,0 +1,13 @@
+package com.flow.file_extension_block;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FileExtensionBlockApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FileExtensionBlockApplication.class, args);
+	}
+
+}
