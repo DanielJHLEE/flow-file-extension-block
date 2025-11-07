5832e1201076915954cb648eb5e90ed0dd28779d
leeJH <ljh0200@outlook.kr>
2025-11-07 15:04:04 +0900

    fix: 확장자 view컨트롤러 맵핑 주소 오류 수정

--- src/main/java/com/flow/file_extension_block/domain/ext_file/controller/ExtFileViewController.java
+++ src/main/java/com/flow/file_extension_block/domain/ext_file/controller/ExtFileViewController.java
@@ -4,7 +4,7 @@ import com.flow.file_extension_block.domain.ext_file.service.ExtFilePolicyServic
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
  * 확장자 뷰 컨트롤러
@@ -16,7 +16,7 @@ public class ExtFileViewController {
 
     private final ExtFilePolicyService extensionService;
 
-    @GetMapping("/extensions")
+    @RequestMapping("/api/extensions")
     public String viewExtensions(Model model) {
         model.addAttribute("fixedList", extensionService.findByType("FIXED"));
         model.addAttribute("customList", extensionService.findByType("CUSTOM"));
